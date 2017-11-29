f881cf701508d696015b66bc9a1c24b0107b78d6
Thanasis Tryfonos <thanasistryfonos@gmail.com>
2017-11-29 12:38:48 +0200

    Added weighted load balancing front-end

--- build_run_containers.sh
+++ build_run_containers.sh
@@ -8,12 +8,15 @@ docker network rm envoymesh
 docker build -t jservice . -f Dockerfile-jservice
 docker build -t front-envoy . -f Dockerfile-frontenvoy
 docker build -t goservice . -f Dockerfile-goservice
+docker build -t front-envoy-weighted . -f Dockerfile-frontenvoy-weighted
+
 
 # Build docker envoy network mesh
 docker network create -d bridge envoymesh
 
 # Run Containers
 docker run -d -p 8000:80 -p 9001:9001 --network=envoymesh --net-alias=frontenvoy --name front-envoy front-envoy
+docker run -d -p 8010:80 -p 9010:9010 --network=envoymesh --net-alias=frontenvoy-weighted --name front-envoy-weighted front-envoy-weighted
 docker run -d --network=envoymesh --net-alias=jservice --name jservice jservice
 docker run -d --network=envoymesh --net-alias=goservice --name goservice goservice
 
